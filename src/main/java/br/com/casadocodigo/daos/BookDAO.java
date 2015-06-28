4ef929a277331ac74ddbdbaa145e7877f37a8c35
asouza <alots.ssa@gmail.com>
2015-06-28 19:56:16 -0300

    [fechando compra] pagina de detalhe do livro

--- src/main/java/br/com/casadocodigo/daos/BookDAO.java
+++ src/main/java/br/com/casadocodigo/daos/BookDAO.java
@@ -7,27 +7,37 @@ import javax.persistence.PersistenceContext;
 
 import br.com.casadocodigo.models.Book;
 
-
 public class BookDAO {
-	
+
 	@PersistenceContext
 	private EntityManager manager;
-		
+
 	public void save(Book product) {
 		manager.persist(product);
 	}
 
 	public List<Book> list() {
-		return manager.createQuery("select b from Book b join fetch b.authors",Book.class).getResultList();
+		return manager.createQuery("select b from Book b join fetch b.authors",
+				Book.class).getResultList();
 	}
 
 	public List<Book> lastReleases() {
-		return manager.createQuery("select b from Book b where b.releaseDate <= now() order by b.id desc",Book.class).setMaxResults(3).getResultList();
+		return manager
+				.createQuery(
+						"select b from Book b where b.releaseDate <= now() order by b.id desc",
+						Book.class).setMaxResults(3).getResultList();
 	}
 
 	public List<Book> olderBooks() {
-		return manager.createQuery("select b from Book b",Book.class).setMaxResults(20).getResultList();
+		return manager.createQuery("select b from Book b", Book.class)
+				.setMaxResults(20).getResultList();
 	}
 
-	
+	public Book findById(Integer id) {
+		return manager
+				.createQuery(
+						"select b from Book b join fetch b.authors where b.id=:id",
+						Book.class).setParameter("id", id).getSingleResult();
+	}
+
 }
