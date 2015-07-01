68c1507822c960ade63ba89989970039312506b3
asouza <alots.ssa@gmail.com>
2015-07-01 10:42:06 -0300

    [integracao] deixando o pagamento assincrono

--- src/main/java/br/com/casadocodigo/resources/PaymentResource.java
+++ src/main/java/br/com/casadocodigo/resources/PaymentResource.java
@@ -1,32 +1,66 @@
 package br.com.casadocodigo.resources;
 
+import java.math.BigDecimal;
 import java.net.URI;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-import javax.ws.rs.GET;
+import javax.inject.Inject;
+import javax.servlet.ServletContext;
+import javax.ws.rs.ClientErrorException;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
 import javax.ws.rs.container.AsyncResponse;
 import javax.ws.rs.container.Suspended;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriBuilder;
 
+import br.com.casadocodigo.models.PaymentData;
+import br.com.casadocodigo.models.ShoppingCart;
 
 @Path("payment")
 public class PaymentResource {
-	
-	
-	private ExecutorService executor = Executors.newFixedThreadPool(50);
 
-	@GET @Path("/async/teste")
-    public void longRunningOp(@Suspended final AsyncResponse ar) {		
-        executor.submit( () -> {
-                    try {
-						Thread.sleep(3000);
-					} catch (Exception e) {
-						throw new RuntimeException(e);
-					}
-                    ar.resume(Response.seeOther(URI.create("http://localhost:8080/casadocodigo/site/carrinho.xhtml")));
-        });
+	private ExecutorService executor = Executors.newFixedThreadPool(50);
+	@Inject
+	private ShoppingCart cart;
+	@Context
+	private ServletContext ctx;
+
+	@POST
+	@Path("/")
+	public void pay(@Suspended final AsyncResponse ar) {
+		executor.submit(() -> {
+
+			BigDecimal total = cart.getTotal();
+			String uriToPay = "http://book-payment.herokuapp.com/payment";
+
+			Client client = ClientBuilder.newClient();
+			PaymentData paymentData = new PaymentData(total);
+			try {
+				client.target(uriToPay).request()
+						.post(Entity.json(paymentData), String.class);
+
+				URI redirectURI = UriBuilder
+						.fromUri(ctx.getContextPath()+"/site/index.xhtml")
+						.queryParam("msg", "Compra realizada com sucesso")
+						.build();
+
+				Response response = Response.seeOther(redirectURI).build();
+
+				ar.resume(response);
+				
+			} catch (ClientErrorException exception) {
+				exception.printStackTrace();
+				ar.resume(exception);
+			} catch (Exception e) {
+				ar.resume(e);
+			}
+		});
 	}
-	
+
 }
