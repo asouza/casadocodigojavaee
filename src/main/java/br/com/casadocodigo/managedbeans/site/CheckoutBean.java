e26d591324a9e1d7656f43a971a02aa6cbcc1f8d
asouza <alots.ssa@gmail.com>
2015-07-02 12:01:51 -0300

    [fechando compra] cadastro de usuario

--- src/main/java/br/com/casadocodigo/managedbeans/site/CheckoutBean.java
+++ src/main/java/br/com/casadocodigo/managedbeans/site/CheckoutBean.java
@@ -11,12 +11,22 @@ import javax.ws.rs.client.Entity;
 
 import br.com.casadocodigo.models.PaymentData;
 import br.com.casadocodigo.models.ShoppingCart;
+import br.com.casadocodigo.models.SystemUser;
 
 @Model
-public class PaymentBean {
+public class CheckoutBean {
 
 	@Inject
 	private ShoppingCart cart;
+	private SystemUser systemUser = new SystemUser();
+
+	public SystemUser getSystemUser() {
+		return systemUser;
+	}
+
+	public void setSystemUser(SystemUser systemUser) {
+		this.systemUser = systemUser;
+	}
 
 	public String checkout() {
 		BigDecimal total = cart.getTotal();
@@ -26,12 +36,11 @@ public class PaymentBean {
 		PaymentData paymentData = new PaymentData(total);
 		try {
 			client.target(uriToPay).request()
-				.post(Entity.json(paymentData), String.class);
+					.post(Entity.json(paymentData), String.class);
 			return "/site/pagamento/ok.xhtml?faces-redirect=true";
-		} catch (ClientErrorException exception){
+		} catch (ClientErrorException exception) {
 			return "/site/pagamento/falha.xhtml?faces-redirect=true";
 		}
 
-		
 	}
 }
---
+++ src/main/java/br/com/casadocodigo/models/SystemUser.java
@@ -0,0 +1,125 @@
+package br.com.casadocodigo.models;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotBlank;
+
+public class SystemUser {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	@Email
+	@NotBlank
+	private String email;
+	@NotBlank
+	private String firstName;
+	@NotBlank
+	private String lastName;
+	@NotBlank
+	private String socialId;
+	@NotBlank
+	private String address;
+	@NotBlank
+	private String city;
+	@NotBlank
+	private String state;
+	@NotBlank
+	private String postalCode;
+	@NotBlank
+	private String phone;
+	@NotBlank
+	private String country;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getSocialId() {
+		return socialId;
+	}
+
+	public void setSocialId(String socialId) {
+		this.socialId = socialId;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getPostalCode() {
+		return postalCode;
+	}
+
+	public void setPostalCode(String postalCode) {
+		this.postalCode = postalCode;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+}
