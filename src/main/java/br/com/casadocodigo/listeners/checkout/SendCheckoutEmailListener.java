70c6bfb296fddd238faf6e4115ec1f1fb3b4d7c3
asouza <alots.ssa@gmail.com>
2015-07-05 16:14:28 -0300

    [jms] configurando listeners corretamente

--- src/main/java/br/com/casadocodigo/listeners/checkout/SendCheckoutEmailListener.java
+++ src/main/java/br/com/casadocodigo/listeners/checkout/SendCheckoutEmailListener.java
@@ -1,5 +1,32 @@
 package br.com.casadocodigo.listeners.checkout;
 
-public class SendCheckoutEmailListener {
+import javax.ejb.ActivationConfigProperty;
+import javax.ejb.MessageDriven;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.TextMessage;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@MessageDriven(activationConfig = {
+		@ActivationConfigProperty(
+				propertyName = "destinationLookup", 
+				propertyValue = "java:/jms/topics/checkoutsTopic") 
+		})
+public class SendCheckoutEmailListener implements MessageListener{
+	
+	private Logger logger = LoggerFactory.getLogger(SendCheckoutEmailListener.class);
+
+	@Override
+	public void onMessage(Message message) {
+		TextMessage text = (TextMessage) message;
+		try {
+			System.out.println("Enviando email "+text.getText());
+		} catch (JMSException e) {
+			logger.error("Problema no envio do email",e);
+		}
+	}
 
 }
