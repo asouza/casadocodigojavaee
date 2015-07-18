a411107306211f601534ce81f0a3db9d35f9caea
asouza <alots.ssa@gmail.com>
2015-07-18 18:34:55 -0300

    [seguranca] carregando todas as informacoes do usuario

--- src/main/java/br/com/casadocodigo/security/SecurityDAO.java
+++ src/main/java/br/com/casadocodigo/security/SecurityDAO.java
@@ -5,7 +5,6 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
@@ -13,13 +12,13 @@ import org.springframework.stereotype.Component;
 import br.com.casadocodigo.models.SystemUser;
 
 @Component
-public class SystemUserDAO implements UserDetailsService{
+public class SecurityDAO implements UserDetailsService{
 
 	@PersistenceContext
 	private EntityManager em;
 
 	@Override
-	public UserDetails loadUserByUsername(String username)
+	public SystemUser loadUserByUsername(String username)
 			throws UsernameNotFoundException {
 		String jpql = "select u from SystemUser u where u.email = :login";
 		List<SystemUser> users = em.createQuery(jpql,SystemUser.class).setParameter("login", username).getResultList();
