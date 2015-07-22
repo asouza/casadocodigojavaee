85f237b12b32b493fcdc6bb7408d09bb9b26233e
asouza <alots.ssa@gmail.com>
2015-07-22 11:30:45 -0300

    [integracao] caso tenha algum problema, retorna WebApplicationException

--- src/main/java/br/com/casadocodigo/resources/PaymentResource.java
+++ src/main/java/br/com/casadocodigo/resources/PaymentResource.java
@@ -14,6 +14,7 @@ import javax.transaction.Transactional;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.container.AsyncResponse;
 import javax.ws.rs.container.Suspended;
 import javax.ws.rs.core.Context;
@@ -62,7 +63,7 @@ public class PaymentResource {
 				ar.resume(response);
 
 			} catch (Exception exception) {
-				ar.resume(exception);
+				ar.resume(new WebApplicationException(exception));
 			}
 		});
 	}
